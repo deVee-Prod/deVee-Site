b1c6939a102748103129c246ddad9aa9df50e15d
deVee Studio <davidbendavid@deVees-MacBook-Pro.local>
2026-05-03 15:47:00 +0300

    Add Spotify playlist section with Prod by. deVee embed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { HeroSection } from './components/HeroSection';
 import { NewsBanner } from './components/NewsBanner';
 import { VideoSection } from './components/VideoSection';
 import { UtilitiesSection } from './components/UtilitiesSection';
+import { SpotifySection } from './components/SpotifySection';
 import { SocialSection } from './components/SocialSection';
 import { Footer } from './components/Footer';
 import { InfoPage } from './pages/InfoPage';
@@ -28,6 +29,7 @@ function App() {
         <NewsBanner />
         <VideoSection />
         <UtilitiesSection />
+        <SpotifySection />
         <SocialSection />
       </main>
       <Footer />
