7bbdbee16c73a933fee3c4fb1797820d6b0b4066
David Ben David <davidbn891@gmail.com>
2026-06-15 18:01:19 +0300

    revert: remove visible FAQ section and fix meta tags to strictly professional/invisible

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,7 +2,6 @@ import { Header } from '../components/Header';
 import { HeroSection } from '../components/HeroSection';
 import { VideoSection } from '../components/VideoSection';
 import { SectionSeparator } from '../components/SectionSeparator';
-import { FAQSection } from '../components/FAQSection';
 import { ContactSection } from '../components/ContactSection';
 import { Footer } from '../components/Footer';
 import { DriftingGridBackground } from '../components/DriftingGridBackground';
@@ -17,7 +16,6 @@ export function LandingPage() {
           <HeroSection />
           <VideoSection />
           <SectionSeparator />
-          <FAQSection />
           <ContactSection />
         </main>
         <Footer />
