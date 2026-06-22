15ce5f14d3b70d76bc682f85a8507140574d312c
David Ben David <davidbn891@gmail.com>
2026-06-22 23:30:27 +0300

    style: finely adjust mobile news banner position

--- frontend/src/components/NewsBanner.tsx
+++ frontend/src/components/NewsBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const NewsBanner = () => {
   return (
-    <section className="w-full flex justify-center pt-12 pb-12 md:py-12">
+    <section className="w-full flex justify-center pt-16 pb-12 md:py-12">
       <div className="w-[85%] max-w-[1200px] border border-white/10 overflow-hidden shadow-2xl">
         <img
           src="/assets/news-may.jpeg"
