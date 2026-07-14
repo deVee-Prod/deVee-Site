dfbedf678438d5f699dd4575d5c87d938a195665
David Ben David <davidbn891@gmail.com>
2026-07-14 16:05:27 +0300

    feat: remove news banner section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { router } from './router';
 import { Header } from './components/Header';
 import { HeroSection } from './components/HeroSection';
-import { NewsBanner } from './components/NewsBanner';
 import { VideoSection } from './components/VideoSection';
 import { UtilitiesSection } from './components/UtilitiesSection';
 import { PluginsSection } from './components/PluginsSection';
@@ -29,7 +28,6 @@ function App() {
         <Header />
         <main>
           <HeroSection />
-          <NewsBanner />
           <VideoSection />
           <UtilitiesSection />
           <PluginsSection />
